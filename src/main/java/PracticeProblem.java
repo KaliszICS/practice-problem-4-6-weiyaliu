222706188118fba2eb7ed96918e6456dbbbd8b39
weiyaliu <343445839@tdsb.ca>
2026-04-24 03:04:37 +0000

    yay

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/**
+	* File: Lesson 4.6 - Data type checking
+	* Author: Mr. Kalisz
+	* Date Created: April 12, 2023
+	* Date Last Modified: April 15, 2026
+	*/
+
 import java.util.Scanner;
 
 public class PracticeProblem {
@@ -6,4 +13,85 @@ public class PracticeProblem {
 
 	}
 
+	public static int intChecker() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Input an integer: ");
+		
+		int num;
+
+		while (!(input.hasNextInt())) {
+			input.nextLine();
+			System.out.println("Input an integer: ");
+		}
+		
+		num = input.nextInt();
+		
+		return num;		
+	}	
+
+	public static double doubleChecker() {
+		
+		Scanner input = new Scanner(System.in);
+
+
+		double num;
+
+		System.out.println("Input a number: ");
+
+
+		while (!(input.hasNextDouble())) {
+			input.nextLine();
+			System.out.println("Input a number: ");
+		}
+		
+		num = input.nextDouble();
+		
+		return num;
+	}
+
+	public static boolean booleanChecker() {
+		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Input a boolean: ");
+
+		boolean bool;
+
+		while (!(input.hasNextBoolean())) {
+			input.nextLine();
+			
+			System.out.println("Input a boolean: ");
+
+		}
+
+		bool = input.nextBoolean();
+
+		return bool;
+	}
+
+	public static boolean multipleIntChecker() {
+
+		Scanner input = new Scanner(System.in);
+
+		int num;
+
+		for (int i = 0; i < 4; i++ ) {
+			
+			System.out.println("Input an integer: ");
+
+
+			while (!(input.hasNextInt())) {
+				input.nextLine();
+				System.out.println("Input an integer: ");
+			}
+
+			num = input.nextInt();
+			 
+		}
+
+		return true;		
+
+	}
 }
